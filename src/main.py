6a577f7493e50f5ca20910c1eb7248f4fb44dce1
Jankie <qwq@awaish.link>
2024-02-10 16:43:17 +0800

    Add tips

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import maths.bayes
 
 def main() -> int:
     print('- Probability-Roulette -')
-    print('Use probabilistic Buckshot Roulette to win.')
+    print('Use probabilistic Buckshot Roulette to win. (Use Spaces to separate.)')
     B, E, S = map(int, input('Total bullets, Number of blanks, Hit count: ').split())
     
 if __name__ == '__main__':
