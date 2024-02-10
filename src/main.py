746cfcc3a978a7203a9adaf2ebbefafbe1ded1b0
Jankie <qwq@awaish.link>
2024-02-10 16:38:40 +0800

    New function main

---
+++ src/main.py
@@ -0,0 +1,8 @@
+import maths.bayes
+
+def main() -> int:
+    print('- Probability-Roulette -')
+    print('Use probabilistic Buckshot Roulette to win.')
+    B = input('Total bullets:')
+    E = input('Number of blanks:')
+    S = input('Hit count:')
