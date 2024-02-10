e1e9c07f42591d65a5c0d587361e3d55ef36c8dd
Jankie <qwq@awaish.link>
2024-02-10 16:44:21 +0800

    Add return information

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
-import maths.bayes
+from maths.bayes import *
 
 def main() -> int:
     print('- Probability-Roulette -')
     print('Use probabilistic Buckshot Roulette to win. (Use Spaces to separate.)')
     B, E, S = map(int, input('Total bullets, Number of blanks, Hit count: ').split())
-    
+    R = bayes(B,E,S)
+    print('Result: {}'.format(R))
 if __name__ == '__main__':
     raise SystemExit(main())
