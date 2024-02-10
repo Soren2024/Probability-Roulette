2389c19d11458ee95d52c227eee75c2a655f4a6d
Jankie <qwq@awaish.link>
2024-02-10 16:40:27 +0800

    Optimized input

--- src/main.py
+++ src/main.py
@@ -3,6 +3,4 @@ import maths.bayes
 def main() -> int:
     print('- Probability-Roulette -')
     print('Use probabilistic Buckshot Roulette to win.')
-    B = input('Total bullets:')
-    E = input('Number of blanks:')
-    S = input('Hit count:')
+    B, E, S = map(int, input('Total bullets, Number of blanks, Hit count: ').split())
