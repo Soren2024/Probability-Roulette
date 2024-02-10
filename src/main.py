257dbddedc38292e8488dd4776301f5ab73ef7d4
Soren2024 <jankie2023@outlook.com>
2024-02-10 17:13:18 +0800

    Use the more standard import method

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
-from maths.bayes import *
+import maths.bayes
 
 def main() -> int:
     print('- Probability-Roulette -')
     print('Use probabilistic Buckshot Roulette to win. (Use Spaces to separate.)')
     B, E, S = map(int, input('Total bullets, Number of blanks, Hit count: ').split())
-    R = bayes(B,E,S)
+    R = maths.bayes.bayes(B,E,S)
     print('Result: {}'.format(R))
 if __name__ == '__main__':
     raise SystemExit(main())
